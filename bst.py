8104e9c48d84689d466dfa4990341bf0ea7e893c
Nathan St. John <natestjohn05@gmail.com>
2025-06-02 00:03:43 -0700

    Fixes

--- bst.py
+++ bst.py
@@ -3,6 +3,9 @@ import unittest
 from typing import *
 from dataclasses import dataclass
 sys.setrecursionlimit(10**6)
+import unittest.util
+
+unittest.util._MAX_LENGTH = int(1e6)
 
 BinTree: TypeAlias = Union[None, "Node"]
 
@@ -49,7 +52,42 @@ class frozenBinarySearchTree:
                     return self.lookup(val, l)
                 else:
                     return self.lookup(val, r)
+    
+    # Delete node while retaining binary structure.
+    def delete(self, val: Any, cur_tree: BinTree = "BR") -> BinTree:
+        if cur_tree == "BR":
+            cur_tree = self.tree
+        match cur_tree:
+            case None:
+                return False
+            case Node(v, l, r):
+                if not self.comes_before(v, val) and not self.comes_before(val, v):
+                    if l == None and r == None:
+                        return None
+                    elif l == None: 
+                        return r
+                    elif r == None:
+                        return l
+                    else:
+                        return Node(self.rightmost(l), self.remove_rightmost(l), r)
+                elif self.comes_before(v, val):
+                    return Node(v, self.delete(val, l), r)
+                else:
+                    return Node(v, l, self.delete(val, r))
 
+    # Find rightmost node of sub tree
+    def rightmost(self, tree: BinTree) -> Any:
+        if tree.right == None:
+            return tree.val
+        else:
+            return self.rightmost(tree.right)
+        
+    # Remove rightmost node of sub tree
+    def remove_rightmost(self, tree: BinTree) -> BinTree:
+        if tree.right == None:
+            return None
+        else:
+            return Node(tree.val, tree.left, self.remove_rightmost(tree.right))
 
 class TestCase(unittest.TestCase):
     def test_none(self):
@@ -71,7 +109,6 @@ class TestCase(unittest.TestCase):
         tr = frozenBinarySearchTree(tr.insert(2), lambda a, b : a > b)
         tr = frozenBinarySearchTree(tr.insert(7), lambda a, b : a > b)
         self.assertEqual(Node(5,Node(3,Node(2,None,None),None),Node(10,Node(7,None,None),None)), tr.tree)
-
         self.assertEqual(tr.lookup(9), False)
         self.assertEqual(tr.lookup(5), True)
         self.assertEqual(tr.lookup(3), True)
@@ -80,12 +117,14 @@ class TestCase(unittest.TestCase):
         self.assertEqual(tr.lookup(10), True)
         self.assertEqual(tr.lookup(-1), False)
         self.assertEqual(tr.lookup(10000), False)
+        self.assertEqual(tr.rightmost(tr.tree), 10)
+        self.assertEqual(tr.delete(10), Node(5,Node(3,Node(2,None,None),None),Node(7,None,None)))
+        self.assertEqual(tr.delete(3), Node(5,Node(2,None,None),Node(10,Node(7,None,None),None)))
+        self.assertEqual(tr.delete(5), Node(3,Node(2,None,None),Node(10,Node(7,None,None),None)))
+
 
 
 if __name__ == '__main__':
     unittest.main()
 
-new_class = frozenBinarySearchTree(Node(5, Node(4, None, None), Node(6, None, None)), lambda a, b : a > b)
-print(Node(5, Node(4, None, None), Node(6, None, Node(8, None, None))))
-print(new_class.insert(8))
 
